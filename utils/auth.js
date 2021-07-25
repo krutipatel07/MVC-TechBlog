f70da514fd1bb3d998b559ec03d74f335b63266f
Kruti Patel <kruti.patel0709@gmail.com>
2021-07-25 14:37:49 -0400

    edit utils

--- utils/auth.js
+++ utils/auth.js
@@ -1,12 +1,9 @@
 const withAuth = (req, res, next) => {
-    // request callback fx checking for session property
   if (!req.session.user_id) {
-    res.redirect("/login");
+      res.redirect('/login');
   } else {
-    //   if res.session.user_id exists, call next()
-    // another middleware or final fx that renders template
-    next();
-  }
+      next();
+  };
 };
 
 module.exports = withAuth;
