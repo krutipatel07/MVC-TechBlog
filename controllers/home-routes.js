3a0289e182174c7aef1cdfa9f490ee3a1bbddcf1
Kruti Patel <kruti.patel0709@gmail.com>
2021-07-25 14:29:19 -0400

    create home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,88 +1,113 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
-// get all posts for homepage
-router.get("/", (req, res) => {
-  console.log("======================");
-  Post.findAll({
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      // pass single post obj into homepage template
-      // because we're using template engine, use res.render()
-      // specify which template we want to use (homepage.handlebars), .handlebars ext implied
-      //   loop over and map each Sequelize obj into serialized version, saving results in a new posts array
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      //   serialize obj down to properties you need with get()
-      // this is what res.json() does automatically
-      //   .render() can accept array or obj
-      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        .then(dbPostData => {
+            // .get method creates serialized array
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-// get a single post
-router.get("/post/:id", (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
         },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-      //   serialize data
-      const post = dbPostData.get({ plain: true });
-
-      //   pass data to template with session variable
-      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        .then(dbPostData => {
+            if (!dbPostData) { 
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            // serialize the data and pass to template
+            const post = dbPostData.get({ plain: true });
+            
+            res.render('single-post', { 
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect("/");
-      return;
+        res.redirect('/');
+        return;
     }
-    // login doesn't need variables so no second argument to render()
-    res.render("login");
-  });
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,14 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
 
-const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes");
-const dashboardRoutes = require("./dashboard-routes");
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
-// collect packaged group of API endpoints and prefix with /api
-router.use("/", homeRoutes);
-router.use("/dashboard", dashboardRoutes);
-router.use("/api", apiRoutes);
-
-// // if we make a request to any endpoint that doesn't exist
-// router.use((req, res) => {
-//     // error message indicates requested incorrect resource
-//     res.status(404).end();
-// });
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
